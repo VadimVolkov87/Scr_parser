b8c7d0329c9546d982682574bfd50f813ca087a6
Vadim Volkov <bcdy2146@yandex.ru>
2024-10-31 21:40:27 +0300

    Project after 5review

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -11,28 +11,34 @@ DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
 class PepParsePipeline:
     """Класс пайплайна для обработки результатов парсинга."""
 
-    def open_spider(self, spider):
-        """Функция создания словаря для результатов."""
+    @classmethod
+    def from_crawler(cls, crawler):
+        """Метод класса для создания директории результатов."""
         results_dir = BASE_DIR / RESULTS
         results_dir.mkdir(exist_ok=True)
+        return cls()
+
+    def open_spider(self, spider):
+        """Метод создания словаря для результатов."""
         self.results = defaultdict(int)
 
     def process_item(self, item, spider):
-        """Функция подсчета статусов."""
+        """Метод подсчета статусов."""
         self.results[item['status']] += 1
         return item
 
     def close_spider(self, spider):
-        """Функция записи результатов в файл."""
+        """Метод записи результатов в файл."""
         now_formatted = dt.datetime.now().strftime(DATETIME_FORMAT)
         file_name = f'status_summary_{now_formatted}.csv'
         file_path = BASE_DIR / RESULTS / file_name
         with open(file_path, 'w', encoding='utf-8') as f:
-            csv.writer(f,
-                       dialect=csv.unix_dialect,
-                       quoting=csv.QUOTE_NONE
-                       ).writerows((
-                           ('Статус', 'Количество'),
-                           *self.results.items(),
-                           ('Итого', sum(self.results.values())),
-                       ))
+            csv.writer(
+                f,
+                dialect=csv.unix_dialect,
+                quoting=csv.QUOTE_NONE
+                ).writerows((
+                    ('Статус', 'Количество'),
+                    *self.results.items(),
+                    ('Итого', sum(self.results.values())),
+                ))
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -12,12 +12,12 @@ class PepSpider(scrapy.Spider):
     start_urls = [f'https://{domain}/' for domain in allowed_domains]
 
     def parse(self, response):
-        """Функция парсинга ссылок на страницы Pep."""
+        """Метод парсинга ссылок на страницы Pep."""
         for pep_link in response.css('tbody tr td a::attr(href)').getall():
             yield response.follow(pep_link, callback=self.parse_pep)
 
     def parse_pep(self, response):
-        """Функция для парсинга страниц Pep."""
+        """Метод для парсинга страниц Pep."""
         number_names = ' '.join(t.strip() for t in response.css(
             'h1.page-title::text'
         ).getall()).strip().split(' – ')
